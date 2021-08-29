ebd4dbfa02c89a4fe4210f71c316d66c046b6f56
Francisco dos Passos <chico.81@hotmail.com>
2021-08-29 18:28:34 -0300

    remove deprecated dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
@@ -9,8 +8,8 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // const produto = [];
 // const addProduto = (nome, descricao, status) => {
